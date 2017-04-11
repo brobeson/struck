fa050b7ffd15a1fabf70fa3bd22400a2c15ca116
brobeson <brobeson@users.noreply.github.com>
2017-04-11 13:20:51 -0600

    add a bash script to rip through all the data
    The script has a list of sequences. For each sequence, it modifies the configuration file, then runs
    struck.

---
+++ run_experiments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set up the struck configuration options for all sequences
+config_file="config.txt"
+sed --in-place 's/#\?quietMode.*$/quietMode = 1/' ${config_file}
+sed --in-place 's/#\?debugMode.*$/debugMode = 0/' ${config_file}
+sed --in-place 's|#\?sequenceBasePath.*$|sequenceBasePath = /home/brendan/Videos/struck_data|' ${config_file}
+sed --in-place 's/#\?searchRadius.*$/searchRadius = 30/' ${config_file}
+sed --in-place 's/#\?svmC.*$/svmC = 100.0/' ${config_file}
+sed --in-place 's/#\?svmBudgetSize.*$/svmBudgetSize = 100/' ${config_file}
+
+sequences=("coke11" "david" "faceocc" "faceocc2" "girl" "sylv" "tiger1" "tiger2")
+for s in ${sequences[@]}
+do
+    echo "---------------------------------------------------"
+    echo "tracking ${s}..."
+    sed --in-place 's/#\?resultsPath.*$/resultsPath = '${s}'.csv/' ${config_file}
+    sed --in-place 's/#\?sequenceName.*$/sequenceName = '${s}'/' ${config_file}
+    ./struck config.txt
+    echo
+done
